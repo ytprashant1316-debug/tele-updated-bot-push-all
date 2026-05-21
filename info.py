8e7cd0a17770e2740981484a72d3179106059cf5
ytprashant1316-debug <ytprashant1316@gmail.com>
2026-05-21 17:22:40 +0530

    Update bot configuration and environment variables

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://i.ibb.co/MC9cC5f/IMG-20241205-185040-352.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/DPngQS9L/photo-2026-01-16-00-02-20.jpg')).split()
 
 
 # Admins & Users
