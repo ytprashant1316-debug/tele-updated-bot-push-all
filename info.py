0d8cb22f07f184a2f03033462c102d7e70410125
ytprashant1316-debug <ytprashant1316@gmail.com>
2026-05-22 20:29:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/bookreq90')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/freeebookspdf1')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'bookreq90') # Support Chat Link Without https:// or @
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/booksearcher90bot')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/prashantspages')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'booksearcher90bot') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/soniprashant0')
 
 # True Or False
